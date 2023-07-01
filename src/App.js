ca805121806c7c045f85e00775c8fcbd70b3d2de
Jeremy <jejedech.63@gmail.com>
2023-07-01 17:51:21 +1000

    react app installed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div>
+      <div className="bucket-app">
+        <BucketList />
+      </div>
+      <footer>
+        Created by <strong>Jeremy🦈</strong> <br></br>
+        last updated: June 2023 - V2
+      </footer>
+    </div>
+  );
+}
+
+export default App;
