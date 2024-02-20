bf12c9bc983bfdcf2cd1d08c7f48085b4a16e716
Jeremy <jejedech.63@gmail.com>
2024-02-20 21:33:49 +1100

    removing mouse event handler

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,14 +4,7 @@ import { Link } from 'react-router-dom';
 
 const ProjectsPage = () => {
   const [repositories, setRepositories] = useState([]);
-  //let cardKey = 0;
 
-  const handleMouseEnter = (key) => {
-    console.log('Mouse entered the element!'+ (key+1));
-    // You can perform any action or update state here
-  };
-  
-  
   useEffect(() => {
     const fetchRepositories = async () => {
       const apiUrl = 'https://api.github.com/users/Gator2-0/repos';
@@ -40,19 +33,22 @@ const ProjectsPage = () => {
         <h1>Projects</h1>
       </Row>
       <Row>
-        {repositories.map((repo, index) => (
-          <Col key={index+1} onMouseEnter={() => handleMouseEnter(index)}>
-            <Link className='link' to={repo.html_url} target="_blank" rel="noopener noreferrer">
-              <Card className='customCard' >
-                <Card.Title>{repo.name}</Card.Title>
-                <Card.Subtitle>{repo.language}</Card.Subtitle>
-                <Card.Body style={{ maxHeight: '300px', overflowY: 'auto' }} className="custom-scrollbar">
-                  {repo.description}
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-        ))}
+        {repositories.map((repo, index) => {
+          index++
+          return (
+            <Col key={index} >
+              <Link className='link' to={repo.html_url} target="_blank" rel="noopener noreferrer">
+                <Card className='customCard' >
+                  <Card.Title>{repo.name}</Card.Title>
+                  <Card.Subtitle>{repo.language}</Card.Subtitle>
+                  <Card.Body style={{ maxHeight: '300px', overflowY: 'auto' }} className="custom-scrollbar">
+                    {repo.description}
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          )
+        })}
         </Row>
       
     </Container>
