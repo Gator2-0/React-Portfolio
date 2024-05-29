87c0ea69769f5a5952c4c26167ba61ce7ec1b330
Jeremy <jejedech.63@gmail.com>
2024-05-29 20:50:47 +1000

    adding links to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Contact.css';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>My contact here</h1>
+    <div className='main'>
+      <div className='centre'>
+        <a className='contact-details' 
+        href='https://codepen.io/gator2-0' 
+        target="_blank"
+        rel="noopener noreferrer">
+          <h1>codepen account 🖊️</h1>
+        </a>
+        <a className='contact-details' 
+        href='https://github.com/Gator2-0' 
+        target="_blank"
+        rel="noopener noreferrer">
+          <h1>Github account</h1>
+        </a>
+      </div>
     </div>
   );
 };
