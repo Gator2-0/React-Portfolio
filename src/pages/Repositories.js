4eae79833ad3d21da1d6d4bae3349a65656a6677
Jeremy <jejedech.63@gmail.com>
2024-06-02 11:36:20 +1000

    creating repositories specific css file

--- src/pages/Repositories.js
+++ src/pages/Repositories.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import { Container, Row, Col, Card } from 'react-bootstrap';
-//import { Link } from 'react-router-dom';
+import './repositories.css'
 import {handleMouseMove} from '../utils/mouseHandler'
 
 const RepositoriesPage = () => {
