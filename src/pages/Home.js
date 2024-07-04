908af3a29ea6f7534dca0099f40b0521a90d529f
Jeremy <jejedech.63@gmail.com>
2024-07-04 20:30:47 +1000

    styling the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,19 @@ import psScript from '../dominoImages/first-ps-script.png'
 const Home = () => {
   return (
     <section className='home-container'>
-      <p> Scroll down to see my journey 
-      </p>
+      <p> Scroll down to see my journey </p>
+      <div className='story-container'>
+        <div className='story-text'>Coding started with a need. Reducing repetitive tasks and improving efficiency.<br />
+          Simply put: <strong>Making my life easier!</strong> <br />
+          My job at the time required me to go through enormous amount of data to find patern in IT network logs. 
+          This is when I was first introduce to it: <strong> PowerShell</strong>.
+        </div>
+        <div className='story-image'>
+          <img src={psScript} alt='my first powershell script.'></img>
+          
+        </div>
+
+      </div>
      
       
       
