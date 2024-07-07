43b1b75d3c3869ddc1d28552fb302ed1100503b5
Jeremy <jejedech.63@gmail.com>
2024-07-07 18:39:15 +1000

    adding details to about-me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,47 +13,45 @@ const ContactPage = () => {
 
   return (
     <div className='contact-main'>
-      <div className='about-me-container'>
-        <div className='language-list'>
-        {Object.entries(languageImage).map(([language, src]) => (
-        <img
-          key={language}
-          src={process.env.PUBLIC_URL + src}
-          alt={`A ${language} logo`}
-          className="language-logo"
-        />
-      ))}
+      <div className='first-container'>
+        <div className='about-me-container'>
+          <div className='language-list'>
+          {Object.entries(languageImage).map(([language, src]) => (
+          <img
+            key={language}
+            src={process.env.PUBLIC_URL + src}
+            alt={`A ${language} logo`}
+            className="language-logo"
+          />
+        ))}
         </div>
-        <div className='about-me-text'>
+
+        </div>  
+        <div className='contact-container'>
+          <a className='contact-details' 
+          href='https://codepen.io/gator2-0' 
+          target="_blank"
+          rel="noopener noreferrer">
+            <h1>codepen account 🖊️</h1>
+          </a>
+          <a className='contact-details' 
+          href='https://github.com/Gator2-0' 
+          target="_blank"
+          rel="noopener noreferrer">
+            <h1>Github account</h1>
+          </a>
+          <a className='contact-details' 
+          href='https://www.npmjs.com/package/gator-front' 
+          target="_blank"
+          rel="noopener noreferrer">
+            <h1>My NPM package</h1>
+          </a>
+        </div>
+      </div>
+      <div className='about-me-text'>
           I am passionate about learning new skills,understanding how things work and why they don't work.
           Recently I have learnt Python to be able to create an in-house app, I have created an NPM package called <strong>gator-front</strong> used in this very Portfolio.
           And I am eager to be faced with a new challenge just so I can learn something new.
-        </div>
-
-        
-      <div>
-          
-        </div>
-      </div>  
-      <div className='contact-container'>
-        <a className='contact-details' 
-        href='https://codepen.io/gator2-0' 
-        target="_blank"
-        rel="noopener noreferrer">
-          <h1>codepen account 🖊️</h1>
-        </a>
-        <a className='contact-details' 
-        href='https://github.com/Gator2-0' 
-        target="_blank"
-        rel="noopener noreferrer">
-          <h1>Github account</h1>
-        </a>
-        <a className='contact-details' 
-        href='https://www.npmjs.com/package/gator-front' 
-        target="_blank"
-        rel="noopener noreferrer">
-          <h1>My NPM package</h1>
-        </a>
       </div>
     </div>
   );
